fbb3122a746a718efc1ec9c34cd58634fa88eb14
eduardoeleuterio <eduardor.eleuterio@gmail.com>
2023-05-29 11:08:27 -0300

    create the project, add sql connection and first class

---
+++ Data/PataAmigaDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PataAmiga.Models.Domain;
+
+namespace PataAmiga.Data
+{
+    public class PataAmigaDbContext : DbContext
+    {
+        public PataAmigaDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Pet> Pets { get; set; }
+    }
+}
---
+++ Models/Domain/Pet.cs
@@ -0,0 +1,9 @@
+﻿namespace PataAmiga.Models.Domain
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+
+}
